2ed3c63e25e0d3a6693d74632b7707e19ac296c6
Namhyeon Lee <72542667+namhyeonlee@users.noreply.github.com>
2021-04-23 23:51:53 +0900

    Add files via upload

--- js-data/array.js
+++ js-data/array.js
@@ -1,2 +1,16 @@
 const number = [1, 2, 3, 4]
 const fruits = ['apple', 'banana', 'cherry']
+
+const a = fruits.forEach(function (fruit, index) {
+    console.log(`${fruit}-${index}`)
+})
+console.log(a)
+
+const b = fruits.map(function (fruit, index) {
+    return {
+        id: index,
+        name: fruit
+    }
+})
+
+console.log(b)
